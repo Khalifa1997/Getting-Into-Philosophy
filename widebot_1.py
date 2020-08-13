ee8b8b9d1ddd2c417ceb89aadaeb6ad74e0f0d0b
Ahmed Khalifa <ahmed70000@gmail.com>
2020-08-13 02:33:49 +0200

    Update widebot_1.py

--- widebot_1.py
+++ widebot_1.py
@@ -1,4 +1,4 @@
-
+import sys
 import time
 import urllib
 import re
@@ -13,52 +13,55 @@ def getFirst(url):
     response = requests.get(url)
     html = response.text
     soup = bs4.BeautifulSoup(html, "html.parser")
-    #Find content div
+    # Find content div
     content_div = soup.find(
         id="mw-content-text").find(class_="mw-parser-output")
 
     article_link = None
-    found=False
+    found = False
     for element in content_div.find_all("p", recursive=False):
-        #Remove translations/Prononciation
-        txt= element.text.replace("(listen)","")[:150]
-        f= txt[txt.find("(")+1:txt.find(")")]
-        if txt[:-1]==f:
-          #No links between ()
-          f=""
+        # Remove translations/Prononciation
+        txt = element.text.replace("(listen)", "")[:150]
+        f = txt[txt.find("(")+1:txt.find(")")]
+        if txt[:-1] == f:
+            # No links between ()
+            f = ""
         else:
-          #Remove punctuation
-          f= f.replace(":","").replace(",","").replace(".","").replace("'","")
-        for idx,i in enumerate(element.find_all("a", recursive=False)):
-          article_link = i.get('href')
-          #print(article_link)
-          if not article_link:
-            continue
-          if ("Help:" in article_link or "#" in article_link):
-            continue
-          if i.text in f:
-            continue
-          found=True
-          break
-      
+            # Remove punctuation
+            f = f.replace(":", "").replace(
+                ",", "").replace(".", "").replace("'", "")
+        for idx, i in enumerate(element.find_all("a", recursive=False)):
+            article_link = i.get('href')
+            # print(article_link)
+            if not article_link:
+                continue
+            if ("Help:" in article_link or "#" in article_link):
+                continue
+            if i.text in f:
+                continue
+            found = True
+            break
+
         if found:
-          break
+            break
     first_link = urllib.parse.urljoin(
         'https://en.wikipedia.org/', article_link)
 
     return first_link
 
-l=[start_url]
-#https://en.wikipedia.org/wiki/Wikipedia:Dead-end_pages There is no dead-end pages on wikipedia currently
+
+if len(sys.argv) > 1:
+    start_url = str(sys.argv[1])
+l = [start_url]
+# https://en.wikipedia.org/wiki/Wikipedia:Dead-end_pages There is no dead-end pages on wikipedia currently
 print(start_url)
 while(target_url not in l):
-  x= getFirst(l[-1])
-  print(x)
-  if x in l and x != start_url:
-    print("Loop Found")
-    break
-  l.append(x)
-  time.sleep(0.5)
+    x = getFirst(l[-1])
+    print(x)
+    if x in l and x != start_url:
+        print("Loop Found")
+        break
+    l.append(x)
+    time.sleep(0.5)
 if target_url in l:
-  print("Found")
-
+    print("Found")
